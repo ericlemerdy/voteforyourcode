7d055565f10d5d37cacaffc03d4ba954da4a3735
dgageot <david@gageot.net>
2013-07-04 19:36:04 +0200

    Refactor mongo code

--- src/main/java/net/codestory/vote/repository/VoteRepository.java
+++ src/main/java/net/codestory/vote/repository/VoteRepository.java
@@ -9,18 +9,7 @@ public class VoteRepository {
   private final Supplier<MongoCollection> votes;
 
   public VoteRepository(String uri) {
-    votes = Suppliers.memoize(() -> {
-      try {
-        MongoClientURI mongoUri = new MongoClientURI(uri);
-
-        DB db = new MongoClient(mongoUri).getDB(mongoUri.getDatabase());
-        Jongo jongo = new Jongo(db);
-
-        return jongo.getCollection("votes");
-      } catch (Exception e) {
-        throw new IllegalStateException("Unable to read votes", e);
-      }
-    });
+    votes = Suppliers.memoize(() -> connect(uri));
   }
 
   public Iterable<Vote> all() {
@@ -29,5 +18,18 @@ public class VoteRepository {
 
   public void save(Vote vote) {
     votes.get().save(vote);
+  }
+
+  private static MongoCollection connect(String uri) {
+    MongoClientURI mongoUri = new MongoClientURI(uri);
+
+    DB db;
+    try {
+      db = new MongoClient(mongoUri).getDB(mongoUri.getDatabase());
+    } catch (Exception e) {
+      throw new IllegalStateException("Unable to connect to mongo " + uri, e);
+    }
+
+    return new Jongo(db).getCollection("votes");
   }
 }
