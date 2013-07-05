2033f6b3ec5fcadbc29255a67ec99c207686ab8b
dgageot <david@gageot.net>
2013-07-06 00:48:42 +0200

    More choices

--- src/test/java/net/codestory/vote/VoteTest.java
+++ src/test/java/net/codestory/vote/VoteTest.java
@@ -22,21 +22,21 @@ public class VoteTest extends AbstractWebTest {
   @Test
   public void candidates() {
     Random random = getInstance(Random.class);
-    when(random.nextInt(7)).thenReturn(0, 1);
+    when(random.nextInt(44)).thenReturn(0, 1);
 
     goTo("/");
 
-    assertThat(find("#left h2").getText()).contains("FooBarGolo");
-    assertThat(find("#left").getText()).contains("module hello.World");
+    assertThat(find("#left h2").getText()).contains("FooBarQix Java");
+    assertThat(find("#left").getText()).contains("byDivisor(n, 5)");
 
-    assertThat(find("#right h2").getText()).contains("CodeStoryStatusTest");
-    assertThat(find("#right").getText()).contains("extends PhantomJsTest");
+    assertThat(find("#right h2").getText()).contains("FooBarQix Clojure");
+    assertThat(find("#right").getText()).contains("(def number)");
   }
 
   @Test
   public void vote() throws InterruptedException {
     Random random = getInstance(Random.class);
-    when(random.nextInt(7)).thenReturn(0, 1, 0, 1);
+    when(random.nextInt(44)).thenReturn(0, 1, 0, 1);
 
     goTo("/");
 
