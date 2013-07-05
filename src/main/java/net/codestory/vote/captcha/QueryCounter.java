78391f940f2681486541432d5790c8702702c771
dgageot <david@gageot.net>
2013-07-05 16:48:43 +0200

    Better Throttle

--- src/main/java/net/codestory/vote/captcha/QueryCounter.java
+++ src/main/java/net/codestory/vote/captcha/QueryCounter.java
@@ -2,17 +2,24 @@ package net.codestory.vote.captcha;
 
 import static java.util.concurrent.TimeUnit.*;
 
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.*;
 
 import com.google.common.cache.*;
 
 public class QueryCounter {
-  private LoadingCache<String, AtomicLong> queriesPerHost;
+  private final LoadingCache<String, AtomicLong> lastMinuteCountPerHost;
+  private final LoadingCache<String, AtomicLong> lastDayCountPerHost;
 
   public QueryCounter() {
-    this.queriesPerHost = CacheBuilder
+    this.lastMinuteCountPerHost = createCountPerHost(1, MINUTES);
+    this.lastDayCountPerHost = createCountPerHost(1, DAYS);
+  }
+
+  private static LoadingCache<String, AtomicLong> createCountPerHost(long duration, TimeUnit unit) {
+    return CacheBuilder
         .newBuilder()
-        .expireAfterWrite(1, MINUTES)
+        .expireAfterWrite(duration, unit)
         .build(new CacheLoader<String, AtomicLong>() {
           @Override
           public AtomicLong load(String host) {
@@ -21,7 +28,10 @@ public class QueryCounter {
         });
   }
 
-  public long add(String host) {
-    return queriesPerHost.getUnchecked(host).incrementAndGet();
+  public boolean quotaReached(String host) {
+    long countLastMinute = lastMinuteCountPerHost.getUnchecked(host).incrementAndGet();
+    long countLastDay = lastDayCountPerHost.getUnchecked(host).incrementAndGet();
+
+    return (countLastMinute > 30) || (countLastDay > 1000);
   }
 }
--- src/main/java/net/codestory/vote/captcha/ThrottleFilter.java
+++ src/main/java/net/codestory/vote/captcha/ThrottleFilter.java
@@ -16,11 +16,11 @@ public class ThrottleFilter implements Filter {
   @Override
   public boolean apply(HttpExchange exchange) throws IOException {
     String path = exchange.getRequestURI().getRawPath();
+
     if ("/".equals(path) || "/index".equals(path) || "/index.html".equals(path)) {
       String host = exchange.getRemoteAddress().getAddress().toString();
 
-      long count = queryCounter.add(host);
-      if (count > 30) {
+      if (queryCounter.quotaReached(host)) {
         exchange.getResponseHeaders().add("Location", "/captcha");
         exchange.sendResponseHeaders(303, 0);
         return true;
